a53d25d027e04bae29359086126d78f56cce071a
Silverspine1 <samuelbucke@outlook.com>
2026-09-15 14:10:12 +0200

    fixed drive base

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/NewCode/subsystems/Drive/Drivebase/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/NewCode/subsystems/Drive/Drivebase/DriveBase.java
@@ -1,17 +1,11 @@
-package org.firstinspires.ftc.teamcode.CommandBase.old_referance;
+package org.firstinspires.ftc.teamcode.CommandBase.NewCode.subsystems.Drive.Drivebase;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.teamcode.CommandBase.OpModeEX;
 import org.firstinspires.ftc.teamcode.CommandBase.LoopProfiler;
+import org.firstinspires.ftc.teamcode.CommandBase.OpModeEX;
 
 import dev.weaponboy.nexus_command_base.Commands.Command;
 import dev.weaponboy.nexus_command_base.Commands.LambdaCommand;
