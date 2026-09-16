f842ca864143c18dc79cf7d0bdfa3ab002f6cdac
Silverspine1 <samuelbucke@outlook.com>
2026-09-16 13:33:56 +0200

    axon

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/NewCode/subsystems/shooter/AxonEncoder.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.CommandBase.NewCode.subsystems.shooter;
+
+
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.AnalogInputController;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class AxonEncoder {
+
+    AnalogInput encoder;
+    
+
+    public void setOffset(double offset) {
+        this.offset = offset;
+    }
+
+    double offset = 0;
+
+    public void init(HardwareMap hardwareMap, String deviceName){
+        encoder = hardwareMap.get(AnalogInput.class, deviceName);
+    }
+
+    public double getPosition(){
+        return (encoder.getVoltage() / 3.3 * 360) + offset;
+    }
+
+
+}
