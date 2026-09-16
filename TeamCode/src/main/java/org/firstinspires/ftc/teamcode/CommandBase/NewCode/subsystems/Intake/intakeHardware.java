7c9ff32afa88fc483d48a393ef5fb7bb248ecc2c
Kit-Fox <149883596+purplestickyfingers@users.noreply.github.com>
2026-09-16 19:38:55 +0200

    added wait time for ejection but must be changed from a while loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/NewCode/subsystems/Intake/intakeHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandBase/NewCode/subsystems/Intake/intakeHardware.java
@@ -4,11 +4,14 @@ import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.har
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.CommandBase.NewCode.subsystems.Drive.Drivebase.DriveBase;
 
+import dev.weaponboy.nexus_command_base.Commands.Command;
 import dev.weaponboy.nexus_command_base.Hardware.MotorEx;
 import dev.weaponboy.nexus_command_base.Subsystem.SubSystem;
+import dev.weaponboy.nexus_pathing.PathingUtility.RobotPower;
 
 public class intakeHardware extends SubSystem {
 
@@ -21,7 +24,11 @@ public class intakeHardware extends SubSystem {
         transfering
 
     }
+
+    ElapsedTime ReverseTimer = new ElapsedTime();
+    ElapsedTime IntakeTimer = new ElapsedTime();
     intakeState State = intakeState.off;
+    boolean intakeAfterReverse =false;
 
     double currentPower = IMotor.getPower();
 
@@ -59,6 +66,22 @@ public class intakeHardware extends SubSystem {
 
     }
 
+    public void Reverse(boolean reverse, double reverseTime, double intakeTime){
+        while (intakeAfterReverse && (State == intakeState.ejecting || State == intakeState.intaking) && reverse) {
+            if (ReverseTimer.milliseconds() > reverseTime && reverse) {
+                ReverseTimer.reset();
+                State = intakeState.ejecting;
+                reverse = false;
+            } else if (!reverse && ReverseTimer.milliseconds() > reverseTime && State == intakeState.ejecting) {
+                State = intakeState.intaking;
+                IntakeTimer.reset();
+                intakeAfterReverse = true;
+            } else if (intakeAfterReverse && IntakeTimer.milliseconds() > intakeTime) {
+                State = intakeState.off;
+                intakeAfterReverse = false;
+            }
 
+    }
 
+    }
 }
